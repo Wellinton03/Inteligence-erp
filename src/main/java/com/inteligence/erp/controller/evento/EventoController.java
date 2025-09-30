8608b1c739101792925d7ca64050d03978e8db46
Wellinton Silveira <wellinton.silveira@syonet.com>
2025-09-30 15:14:21 -0300

    adição da camada de segurança

--- src/main/java/com/inteligence/erp/controller/evento/EventoController.java
+++ src/main/java/com/inteligence/erp/controller/evento/EventoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/eventos")
+@RequestMapping("/api/evento")
 public class EventoController {
 
     @Autowired
